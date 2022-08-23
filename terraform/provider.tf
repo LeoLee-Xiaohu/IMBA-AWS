da01a0de482451a6f345bd95686f7306af96d789
Grace Bian <grace.bian@outlook.com>
2022-08-23 14:35:34 +1000

    added terraform and workflow

---
+++ terraform/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+    backend "s3" {
+      bucket         = "imba-terraform-state-backend"
+      key            = "terraform.tfstate"
+      region         = "ap-southeast-2"
+      dynamodb_table = "terraform_state"
+    }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.24.0"
+    }
+  }
+}
+
+provider "aws" {}
---
+++ terraform/variable.tf
@@ -0,0 +1,11 @@
+
+variable "bucket_name" {
+  default = "imba-aws-source-code1"
+
+}
+
+variable "data_bucket_name" {
+  default = "imba-aws"
+
+}
+
